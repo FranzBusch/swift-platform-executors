4853f69c8903397a06c1b1479e75def959e37153
Franz Busch <f.busch@apple.com>
2026-09-17 12:32:05 +0200

    [IO] Add experimental API for a TCPSocket scheduler
    In my previous PR, I added the core lifecycle API of an operation scheduler to model the operation state, cancelling and priority escalation. This PR is introduce the surface area for a TCP socket operation scheduler. TCP sockets need a plethora of configuration types around it for the APIs to work. Namely, types to express the socket address, the shutdown direction, and any IOError thrown.
    
    Importantly, this PR does not yet implement these APIs on any platform. This will happen in a subsequent PR.

--- Sources/PlatformExecutors/PThread/Internal/Selector/KQueueSelector.swift
+++ Sources/PlatformExecutors/PThread/Internal/Selector/KQueueSelector.swift
@@ -105,7 +105,7 @@ struct KQueueSelector: ~Copyable {
         nevents: 0,
         timeout: nil
       )
-    } catch let err as IOError {
+    } catch let err as SyscallError {
       if err.errnoCode == EINTR {
         // See https://www.freebsd.org/cgi/man.cgi?query=kqueue&sektion=2
         // When kevent() call fails with EINTR error, all changes in the changelist have been applied.
